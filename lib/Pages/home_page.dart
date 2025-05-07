cdf81bf8f6726552da574f15038b03c2cdbdaa27
carlos0754 <carlosaugusto11907@gmail.com>
2025-05-07 16:19:29 -0300

    atualizações

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:meu_app_flutter_clone/services/sorteador_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,19 +11,31 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var numeroSorteado = 0;
+  var qtdSorteios = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Meu Primeiro App Flutter"),
+        title: Text("Meu Primeiro App Flutter", style: GoogleFonts.pacifico(),),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: Center(child: Text(numeroSorteado.toString())),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "tivemos até agora $qtdSorteios sorteio!",style: GoogleFonts.alef(fontSize: 60),),
+           Text(numeroSorteado.toString(),style: GoogleFonts.alef(fontSize: 100),),
+            Text(
+              "o último número sorteado foi: ___",style: GoogleFonts.alef(fontSize: 50),),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.monetization_on_outlined),
         onPressed: () {
           setState(() {
+            qtdSorteios++;
             numeroSorteado = SorteadorService.sortearNumero();
           });
           debugPrint(numeroSorteado.toString());
